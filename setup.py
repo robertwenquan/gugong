6722bf4ab1bd761f5e2d6457886e33ba30626ca1
Robert Wen <robert.wen@nyu.edu>
2020-06-22 12:09:28 +0800

    add setup.py file for wheel build

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gugong-saphires", # Replace with your own username
+    version="0.5.0",
+    author="Robert Wen",
+    author_email="saphires@163.com",
+    description="Data library for GuGong, a.k.a as The Palace Museum or The Fobidden City",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/robertwenquan/gugong",
+    packages=['gugong'],
+    package_dir={'gugong': 'gugong'},
+    package_data={'gugong': ['data/gugong.json', 'proto/*.py']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
